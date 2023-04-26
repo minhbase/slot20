d35f8afb6eacc4987e0c874bacc33b530c3a6209
minhbase <minhlnth2210022@fpt.edu.vn>
2023-04-26 10:43:20 +0700

    a

---
+++ js/main.js
@@ -0,0 +1,37 @@
+let id=(id)=>document.getElementById(id);
+
+let classes=(classes)=>document.getElementsByClassName(classes);
+
+let username=id("username"),
+email=id("email"),
+password=id("password"),
+form = id("form"),
+errorMsg=classes("error"),
+successIcon=classes("success-icon"),
+failureIcon=classes("failure-icon");
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+
+    engine(username, 0, "Username can not be blank");
+    engine(email, 1, "Email can not be blank");
+    engine(password, 2, "Password can not be blank");
+});
+
+let engine=(id,serial,message) => {
+    if(id.value.trim()==""){
+        errorMsg[serial].innerHTML=message;
+        id.style.border= "2px solid red";
+
+        //icons
+        failureIcon[serial].style.opacity="1";
+        sucessIcon[serial].style.opacity="0";
+    }else{
+        errorMsg[serial].innerHTML="";
+        id.style.border= "2px solid green";
+
+        //icons
+        failureIcon[serial].style.opacity ="0";
+        sucessIcon[serial].style.opacity="1";
+    }
+}
